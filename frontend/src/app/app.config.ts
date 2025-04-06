03caf48c693e044f14e4dacc154b0cba2cae729a
AmiruHoradagoda <amirumithsara1234@gmail.com>
2025-04-07 00:09:52 +0530

    feat: implement httpManagerInterceptor for handling authorization and error management

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,13 +4,20 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { httpManagerInterceptor } from './core/interceptor/http-manager.interceptor';
+import { AuthService } from './core/services/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withFetch(), withInterceptors([httpManagerInterceptor])),
     provideClientHydration(),
     provideAnimationsAsync(),
+    AuthService,
   ],
 };
